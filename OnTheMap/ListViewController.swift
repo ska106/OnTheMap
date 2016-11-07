1da58d1a46a200676c149d83d673ce0cabdf957e
Sudeep Agrawal <Sudeep.Agrawal@gmail.com>
2016-11-06 19:54:46 -0500

    Added the TableView to the storyBoard, prototype Cells and implementation in ListViewController.

--- OnTheMap/ListViewController.swift
+++ OnTheMap/ListViewController.swift
@@ -9,6 +9,82 @@
 import Foundation
 import UIKit
 
-class ListViewController
+class ListViewController:UIViewController, UITableViewDataSource, UITableViewDelegate
 {
+    var parseClient:ParseClient!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        //Get the singleton instances of the API clients.
+        parseClient = ParseClient.sharedInstance
+    }
+    
+    override func viewWillAppear(animated:Bool)
+    {
+        super.viewWillAppear(animated)
+        self.parseClient.getStudentLocation() { success, errorMessage in
+            if success
+            {
+                dispatch_async(dispatch_get_main_queue())
+                {
+                    self.tableView.reloadData()
+                }
+            }
+            else
+            {
+                //Display error message
+                let alert = UIAlertController(title: "Error", message: errorMessage, preferredStyle: UIAlertControllerStyle.Alert)
+                let dismissAction = UIAlertAction(title: "OK", style: .Default)
+                { (action) in
+                }
+                alert.addAction(dismissAction)
+                dispatch_async(dispatch_get_main_queue())
+                {
+                    self.presentViewController(alert, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath)
+    {
+        let location = parseClient.studentLocations[indexPath.row]
+        
+        let app = UIApplication.sharedApplication()
+        if let url = NSURL(string: location.mediaURL)
+        {
+            app.openURL( url )
+        }
+        else
+        {
+            print("ERROR: Invalid url")
+        }
+    }
+    
+    //MARK : We are going to have only  section in this table.
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int
+    {
+        return 1
+    }
+    
+    //MARK : Function that determines the number for Rows in the table. This will be dependent on the record set fetched 
+    //       by the parseClient --> StudentLocations[]
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return parseClient.studentLocations.count
+    }
+    
+    // MARK : Function that defines the content for each cell in the table.
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCellWithIdentifier("studentLocationCell", forIndexPath: indexPath)
+        let location = parseClient.studentLocations[indexPath.row]
+        cell.textLabel?.text = "\(location.firstName) \(location.lastName)"
+        cell.detailTextLabel?.text = location.mediaURL
+        return cell
+    }
 }
